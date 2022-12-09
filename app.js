9e5246becd1a5b5c6db8a51bb69c751dc61397ce
c0831447 <c0831447@mylambton.ca>
2022-12-09 16:17:52 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,57 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var hbs = require('hbs');
+var session = require('express-session');
+
+var index = require('./routes/index');
+
+var app = express();
+
+
+app.set('views', path.join(__dirname, 'views'));
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: true
+}))
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use(function(req, res, next) {
+    res.locals.session = req.session;
+    next();
+});
+
+app.use('/', index);
+
+
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handling
+app.use(function(err, req, res, next) {
+
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
